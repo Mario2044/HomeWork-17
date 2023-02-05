270ecfe6dd444e8d606e8fce236f32a733a07ce0
Mario2044 <116796406+Mario2044@users.noreply.github.com>
2023-02-05 16:29:39 +0200

    fix

--- index.js
+++ index.js
@@ -3,34 +3,34 @@ let declaredVariable;
 let someData = 99;
 let obj = {age: 33};
 
-console.log(typeof 37);
-console.log(typeof obj);
-console.log(typeof someData + " Wisen");
-console.log(typeof true);
-console.log(typeof 3.14);
-console.log(typeof obj.name);
-console.log(typeof 42);
-console.log(typeof "");
-console.log(typeof "bla");
-console.log(typeof Boolean(1));
-console.log(typeof !!1);
-console.log(typeof obj.age);
-console.log(typeof Infinity);
-console.log(typeof NaN); 
-console.log(typeof (someData + " Wisen"));
-console.log(typeof Number("1"));
-console.log(typeof Number("shoe"));
-console.log(typeof `template literal`);
-console.log(typeof "1"); 
-console.log(typeof typeof 1); 
-console.log(typeof false);
-console.log(typeof String(1));
-console.log(typeof undefined);
-console.log(typeof { a: 1 });
-console.log(typeof declaredVariable);
-console.log(typeof [1, 2, 4]);
-console.log(typeof null);
-console.log(typeof "John");
-console.log(typeof [] + '1');
-console.log(typeof 1 + '1');
-console.log(typeof 10 * '2');
+console.log(typeof 37);                    //number
+console.log(typeof obj);                   //object
+console.log(typeof someData + " Wisen");   //number string
+console.log(typeof true);                  //boolean
+console.log(typeof 3.14);                  //number
+console.log(typeof obj.name);              //undefined  !
+console.log(typeof 42);                    //number
+console.log(typeof "");                    //string
+console.log(typeof "bla");                 //string
+console.log(typeof Boolean(1));            //boolean
+console.log(typeof !!1);                   //boolean !
+console.log(typeof obj.age);               //number
+console.log(typeof Infinity);              //number ??????
+console.log(typeof NaN);                   //number
+console.log(typeof (someData + " Wisen")); //string ??????
+console.log(typeof Number("1"));           //number
+console.log(typeof Number("shoe"));        //number
+console.log(typeof `template literal`);    //string
+console.log(typeof "1");                   //string
+console.log(typeof typeof 1);              //string
+console.log(typeof false);                 //boolean
+console.log(typeof String(1));             //string
+console.log(typeof undefined);             //udefined
+console.log(typeof { a: 1 });              //object
+console.log(typeof declaredVariable);      //undefined
+console.log(typeof [1, 2, 4]);             //object
+console.log(typeof null);                  //object ???????
+console.log(typeof "John");                //string
+console.log(typeof [] + '1');              //obj1
+console.log(typeof 1 + '1');               //number1
+console.log(typeof 10 * '2');              //NaN

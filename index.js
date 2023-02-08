49e0a27e4a25c67615b36196c18ebb31acfba3b4
Mario2044 <116796406+Mario2044@users.noreply.github.com>
2023-02-09 01:24:56 +0200

    update index.js

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ console.log(typeof obj);                   //object
 console.log(typeof someData + " Wisen");   //number string
 console.log(typeof true);                  //boolean
 console.log(typeof 3.14);                  //number
-console.log(typeof obj.name);              //undefined  !
+console.log(typeof obj.name);              //undefined  
 console.log(typeof 42);                    //number
 console.log(typeof "");                    //string
 console.log(typeof "bla");                 //string
 console.log(typeof Boolean(1));            //boolean
-console.log(typeof !!1);                   //boolean !
+console.log(typeof !!1);                   //boolean 
 console.log(typeof obj.age);               //number
-console.log(typeof Infinity);              //number ??????
+console.log(typeof Infinity);              //number 
 console.log(typeof NaN);                   //number
-console.log(typeof (someData + " Wisen")); //string ??????
+console.log(typeof (someData + " Wisen")); //string
 console.log(typeof Number("1"));           //number
 console.log(typeof Number("shoe"));        //number
 console.log(typeof `template literal`);    //string
@@ -29,7 +29,7 @@ console.log(typeof undefined);             //udefined
 console.log(typeof { a: 1 });              //object
 console.log(typeof declaredVariable);      //undefined
 console.log(typeof [1, 2, 4]);             //object
-console.log(typeof null);                  //object ???????
+console.log(typeof null);                  //object 
 console.log(typeof "John");                //string
 console.log(typeof [] + '1');              //obj1
 console.log(typeof 1 + '1');               //number1
